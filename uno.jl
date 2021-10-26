81245f0161bf1469bc58cf244c1d65f7c12d4852
bryanfelixg <felix077@umn.edu>
2021-10-26 11:50:46 -0500

    PDE SOLVER

--- uno.jl
+++ uno.jl
@@ -1,2 +1,70 @@
-# Filopofia documents
-# Dos
+#=
+Looking at 
+du/dt = D1(A_yy u + u A_xx) + au^2/v + ubar - au
+dv/dt = D2(A_yy v + v A_xx) + au^2   - βv
+=#
+using LinearAlgebra
+using DifferentialEquations
+using BenchmarkTools
+using Sundials
+using Plots
+# Some constants
+
+p  = (1.0,1.0,1.0,10.0,0.001,100.0) # a,α,ubar,β,D1,D2
+N  = 100
+Axx = Array(Tridiagonal(
+    [1.0 for i in 1:N-1],
+    [-2.0 for i in 1:N],
+    [1.0 for i in 1:N-1]
+    ))
+Ayy = copy(Ax)
+Axx[2,1] = 2.0 
+Axx[end-1,end] = 2.0
+Ayy[1,2] = 2.0
+Ayy[end,end-1] = 2.0
+
+##
+#Some allocation
+Ayyu = zeros(N,N)
+uAxx = zeros(N,N)
+Du = zeros(N,N)
+Ayyv = copy(Ayyu)
+vAxx = copy(uAxx)
+Dv = copy(Du)
+function basic!(dr,r,p,t)
+    a,α,ubar,β,D1,D2 = p
+    u = @view r[:,:,1]
+    v = @view r[:,:,2]
+    du = @view dr[:,:,1] 
+    dv = @view dr[:,:,2]
+    mul!(Ayyu,Ayy,u)
+    mul!(uAxx,u,Axx)
+    mul!(Ayyv,Ayy,v)
+    mul!(vAxx,v,Axx)
+    @. Du = D1*(Ayyu + uAxx)
+    @. Dv = D2*(Ayyv + vAxx)
+    @. du = Du + a*u*u/v + ubar - α*u
+    @. dv = Dv + a*u*u   - β*v
+end
+
+## Initial Conditions
+a,α,ubar,β,D1,D2 = p
+uss = (ubar +β)/α
+vss = (a/β)*uss^2
+r0 = zeros(100,100,2)
+r0[:,:,1] .= uss.+0.1.*rand.()
+r0[:,:,2] .= vss
+
+## Problem
+prob = ODEProblem(basic!,r0,(0.0,500.0),p)
+sol = solve(prob,CVODE_BDF(linear_solver=:GMRES),
+progress=true,
+save_everystep=false) 
+
+##
+X = reshape([i for i in 1:100 for j in 1:100],N,N)
+Y = reshape([j for i in 1:100 for j in 1:100],N,N)
+pyplot()
+p1 = surface(X,Y,sol[end,:,:,1],title = "[A]")
+p2 = surface(X,Y,sol[end,:,:,2],title = "[B]")
+plot(p1,p2,layout=grid(2,1))|>display
